88d6ed87ce82f16954c2cb1db31ca52be2cc4808
Alejandro Gonzalez <alejandro.g.carrillo@hotmail.com>
2020-06-30 21:31:17 -0500

    Add hackerRank problems and basic info

---
+++ Learning/typedef.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+// Complex types
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+
+// Macros
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair    
+
+
+
+int main()
+{
+    using namespace std;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    ll a = 1234567890;
+    ll b = 9876543210;
+
+    cout << a*b;
+    
+    
+    return 0;
+}
